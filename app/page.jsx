63252ff2353875b1510b0ab8219c36c102a46df1
Dylan Baker <dylanmbaker@yahoo.com>
2023-05-06 22:08:35 -0500

    added some content to the landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,29 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
     return (
-        <main>
-            <h1 className=''>Home</h1>
+        <main className='min-h-screen bg-primary'>
+            <header className='z-40 w-screen bg-white border-b border-gray-200 '>
+                <nav className='flex items-center justify-between py-4 pl-2 pr-6 mx-auto max-w-7xl'>
+                    <div className='flex items-center flex-grow basis-0'>
+                        <Link href='/' className='text-lg font-semibold tracking-wider text-primary'>
+                            ProfileLink
+                        </Link>
+                    </div>
+                    <div className='flex items-center justify-end gap-4'>
+                        <Link href='/sign-in'>
+                            <button className=' hover:underline'>Sign In</button>
+                        </Link>
+                        <Link href='/sign-up'>
+                            <button className='px-3 py-1.5 text-sm text-white transition rounded-full bg-primary hover:bg-primary-hover'>Sign Up</button>
+                        </Link>
+                    </div>
+                </nav>
+            </header>
+            <div>
+                {/* <Image src='/iphone.svg' width='0' height='0' sizes='100%' className='w-auto h-96' /> */}
+            </div>
         </main>
     );
 }
--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -3,59 +3,59 @@ import Link from 'next/link';
 
 export default function SignUp() {
     return (
-        <div className='flex flex-col h-screen mx-auto overflow-hidden max-w-7xl sm:flex-row'>
-            <section className='px-4 my-10 w-fill sm:w-6/12'>
-                <div className='mb-10 text-lg font-bold'>
-                    <Link href='/'>ProfileLink</Link>
-                </div>
-                <h1 className='text-2xl font-bold text-center'>Sign up for an Account</h1>
+        <div className='min-h-screen overflow-hidden bg-gray-100'>
+            <header className='z-40 w-screen bg-white border-b border-gray-200 '>
+                <nav className='flex items-center justify-between py-4 pl-2 pr-6 mx-auto max-w-7xl'>
+                    <div className='flex items-center flex-grow basis-0'>
+                        <Link href='/' className='text-lg font-semibold tracking-wider text-primary'>
+                            ProfileLink
+                        </Link>
+                    </div>
+                    <div className='flex items-center justify-end gap-4'>
+                        <Link href='/sign-in'>
+                            <button className=' hover:underline'>Sign In</button>
+                        </Link>
+                        <Link href='/sign-up'>
+                            <button className='px-3 py-1.5 text-sm text-white transition rounded-full bg-primary hover:bg-primary-hover'>Sign Up</button>
+                        </Link>
+                    </div>
+                </nav>
+            </header>
 
-                <form className='flex flex-col gap-4 mt-5'>
-                    <div className='w-full'>
-                        <label className='block pr-4 mb-1' for='name'>
+            <div className='flex items-center justify-center mx-auto max-w-7xl'>
+                <div className='mx-2 right-0 flex flex-col gap-2 p-10 mt-16 bg-white shadow-xl shadow-outline max-w-[550px] w-full drop-shadow-xl rounded-xl'>
+                    <h1 className='mb-3 text-2xl font-semibold'>Sign in to your account</h1>
+                    <h3>
+                        Already have an account?{' '}
+                        <Link href='/sign-in' className='transition text-secondary hover:text-secondary-hover hover:underline'>
+                            Sign in now!
+                        </Link>
+                    </h3>
+                    <div className='w-full mt-6'>
+                        <label className='block pr-4 mb-1.5 font-medium text-sm' for='name'>
                             Name
                         </label>
-                        <input className='w-full p-2 leading-tight text-gray-700 transition bg-gray-100 border-2 border-gray-200 rounded-md appearance-none focus:outline-none focus:border-gray-400 hover:border-gray-300' id='name' type='text' />
+                        <input className='w-full p-2 leading-tight text-gray-700 transition bg-white border border-gray-300 rounded-md appearance-none focus:outline-none focus:border-gray-500 hover:border-gray-400' id='name' type='text' />
                     </div>
 
-                    <div className='w-full'>
-                        <label className='block pr-4 mb-1' for='username'>
+                    <div className='w-full mt-3'>
+                        <label className='block pr-4 mb-1.5 font-medium text-sm' for='username'>
                             Username
                         </label>
-                        <div className='relative flex items-center'>
-                            <div className='absolute mb-1 select-none left-2'>@</div>
-                            <input className='w-full p-2 pl-6 leading-tight text-gray-700 transition bg-gray-100 border-2 border-gray-200 rounded-md appearance-none focus:outline-none focus:border-gray-400 hover:border-gray-300' id='username' type='text' />
-                        </div>
+                        <input className='w-full p-2 leading-tight text-gray-700 transition bg-white border border-gray-300 rounded-md appearance-none focus:outline-none focus:border-gray-500 hover:border-gray-400' id='username' type='text' />
                     </div>
 
-                    <div className='w-full'>
-                        <label className='block pr-4 mb-1' for='password'>
+                    <div className='w-full mt-3'>
+                        <label className='block pr-4 mb-1.5 font-medium text-sm' for='password'>
                             Password
                         </label>
-                        <input className='w-full p-2 leading-tight text-gray-700 transition bg-gray-100 border-2 border-gray-200 rounded-md appearance-none focus:outline-none focus:border-gray-400 hover:border-gray-300' id='password' type='password' />
+                        <input className='w-full p-2 leading-tight text-gray-700 transition bg-white border border-gray-300 rounded-md appearance-none focus:outline-none focus:border-gray-500 hover:border-gray-400' id='password' type='password' />
                     </div>
-
-                    <div className='flex justify-between mt-5'>
-                        <div className='flex items-center justify-between w-full'>
-                            <Link href='/profile/edit'>
-                                <button className='px-8 py-2 text-sm font-medium text-white transition rounded-md bg-primary hover:bg-primary-hover h-min'>Sign Up</button>
-                            </Link>
-
-                            <p>
-                                Already have an account?{' '}
-                                <span className='font-bold cursor-pointer hover:underline text-secondary'>
-                                    <Link href='/sign-in'>Sign in</Link>
-                                </span>
-                            </p>
-                        </div>
-                    </div>
-                </form>
-            </section>
-            <section className='flex justify-center mx-4 align-middle w-fill sm:w-6/12'>
-                <div className='min-w-full m-4 align-middle sm:my-20'>
-                    <Image src='/login.svg' width='0' height='0' sizes='100%' className='w-6/12 h-auto mx-auto sm:w-full' />
+                    <Link href='/profile/edit'>
+                        <button className='w-full px-4 py-2 mt-6 text-sm font-medium text-white transition rounded-md bg-primary hover:bg-primary-hover h-min'>Sign Up</button>
+                    </Link>
                 </div>
-            </section>
+            </div>
         </div>
     );
 }
