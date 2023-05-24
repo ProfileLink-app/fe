159ed240c8f8c56ebc5af9670aea52a023220671
Dylan Baker <dylanmbaker@yahoo.com>
2023-05-23 20:37:44 -0500

    bug fixes

--- app/page.jsx
+++ app/page.jsx
@@ -36,10 +36,10 @@ export default function Home() {
                 </div>
                 <div className='flex justify-center w-full gap-4 mb-20'>
                     <Link href='/sign-in'>
-                        <button className='px-6 py-2 text-xs font-semibold text-white transition-all duration-300 border rounded-md hover:border-gray-300 hover:text-gray-300 sm:text-sm md:text-base'>Sign In</button>
+                        <button className='px-6 py-4 text-xs font-semibold text-white transition-all duration-300 border rounded-md hover:border-gray-300 hover:text-gray-300 sm:text-sm md:text-base'>Sign In</button>
                     </Link>
                     <Link href='/sign-up'>
-                        <button className='px-6 py-2 text-xs font-semibold transition-all duration-300 bg-white rounded-md sm:text-sm md:text-base hover:bg-gray-300'>Create a Free Account</button>
+                        <button className='px-6 py-4 text-xs font-semibold transition-all duration-300 bg-white rounded-md sm:text-sm md:text-base hover:bg-gray-300'>Create a Free Account</button>
                     </Link>
                 </div>
             </div>
